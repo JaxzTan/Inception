919918b5c0dde5fe7011eeff8cef59b3c1909167
JaxzTan <chengsoo99@gmail.com>
2025-09-10 10:24:32 +0800

    not complete

---
+++ srcs/mariadb/mariadb_config.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+if [ -d /var/lib/mysql/$MYSQL_DATABASE ]; then
+	echo "Database already exists"
+else
+	touch /usr/local/bin/init.sql
+	echo "FLUSH PRIVILEGES;
+		CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;
+		CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQLPASSWORD';
+		GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
+		ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';
+		FLUSH PRIVILEGES;" > /usr/local/bin/init.sql
+	mysqld --user=mysql --bootstrap < /usr/local/bin/init.sql
+fi
+
+exec "$@"
